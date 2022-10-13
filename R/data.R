6c15408f6e5a2b59b31f6710f6ce382330fe5c59
David Astling <dpastling@gmail.com>
2022-10-12 22:21:55 -0600

    add offical wordle list of 2,308 words
    - lazy loaded
    - available as a variable called `wordle`

---
+++ R/data.R
@@ -0,0 +1,11 @@
+
+#' The official wordle dictionary
+#'
+#' The official list of 2,308 possible answers in the wordle dictionary. The New
+#' York Times modified the original list of 2,315 words to remove some objectionable
+#' and obscure words and trimmed to 2,308
+#'
+#' @format ## `wordle`
+#' A character vector of 2308 words
+#' @source <https://www.nytimes.com/games/wordle/>
+"wordle"
